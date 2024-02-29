0ed5e3c2b054fcbd29a1da03978686b73721e8e9
kajwestin <kaj.westin@dataductus.se>
2024-02-29 22:45:04 +0100

    initial commit

---
+++ priorityqueue/priorityqueue/MyPriorityQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PriorityQueue<T>
+{
+    private List<Tuple<T, int>> elements = new List<Tuple<T, int>>();
+
+    public void Enqueue(T item, int priority)
+    {
+        elements.Add(Tuple.Create(item, priority));
+    }
+
+    public T Dequeue()
+    {
+        if (elements.Count == 0)
+        {
+            throw new InvalidOperationException("Priority queue is empty");
+        }
+        var maxIndex = 0;
+        for (var i = 0; i < elements.Count; i++)
+        {
+            if (elements[i].Item2 < elements[maxIndex].Item2)
+            {
+                maxIndex = i;
+            }
+        }
+        var item = elements[maxIndex].Item1;
+        elements.RemoveAt(maxIndex);
+        return item;
+    }
+
+    public int Count
+    {
+        get { return elements.Count; }
+    }
+}
---
+++ priorityqueue/priorityqueue/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace priorityqueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pq = new PriorityQueue<string>();
+            
+            pq.Enqueue("four", 4);
+            pq.Enqueue("ten", 10);
+            pq.Enqueue("three", 3);
+            pq.Enqueue("five", 5);
+            pq.Enqueue("six", 6);
+            pq.Enqueue("eighteen", 18);
+            pq.Enqueue("one", 1);
+            pq.Enqueue("seventy", 70);
+            pq.Enqueue("two", 2);
+            
+
+            while (pq.Count > 0)
+            {
+                Console.WriteLine(pq.Dequeue());
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
